2b592fdd2a4f227988fdd7c161e1e9ddec03feb9
João Gustavo <jgbispo20@gmail.com>
2022-01-03 19:58:23 -0300

    Income, Expense and Balance calculated
    Criado a função para calcular e atualizar o saldo final da conta
    Criado a função para calcular e atualizar as despesas e as receitas da
    conta

--- js/script.js
+++ js/script.js
@@ -3,17 +3,10 @@ const dummyTransaction = [
   { id: 2, name: "Passagem GVBus", ammout: -160.0 },
   { id: 3, name: "ICloud", ammout: -3.5 },
   { id: 4, name: "Wizard", ammout: -135.0 },
-  { id: 5, name: "Wizard", ammout: -135.0 },
-  { id: 6, name: "Wizard", ammout: -135.0 },
-  { id: 7, name: "Wizard", ammout: -135.0 },
-  { id: 8, name: "Wizard", ammout: -135.0 },
-  { id: 9, name: "Wizard", ammout: -135.0 },
-  { id: 10, name: "Wizard", ammout: -135.0 },
-  { id: 11, name: "Wizard", ammout: -135.0 },
 ];
 
 // Atualizando a DOM
-const transactionUL = document.querySelector("#transactions"); 
+const transactionUL = document.querySelector("#transactions");
 
 const addTransactionIntoDOM = (transaction) => {
 
@@ -26,17 +19,55 @@ const addTransactionIntoDOM = (transaction) => {
   // Criando o li para adicionar na DOM
   const li = document.createElement("li");
   li.classList.add(CSSClass);
-  li.innerHTML = `${transaction.name} <span>${operator} R$${Math.abs(transaction.ammout)}</span> 
+  li.innerHTML = `${transaction.name} <span>${operator} R$${Math.abs(
+    transaction.ammout
+  )
+    .toFixed(2)
+    .replace(".", ",")}</span> 
   <button class="delete-btn">x</button>`;
 
   // Adicionando a UL
   transactionUL.prepend(li);
 };
 
-// Iniciando a aplicação
+const sumArray = (array) => {
+  return array
+    .reduce((total, number) => total + number, 0)
+    .toFixed(2)
+    .replace(".", ",")
+    .replace("-", "");
+};
 
+// Calculando o saldo final
+const updateBalanceValues = () => {
+  let balance = document.querySelector("#balance");
+  let incomeUI = document.querySelector("#money-plus");
+  let expenseUI = document.querySelector("#money-minus");
+  
+  // Fazendo o calculo
+  const transactionAmounts = dummyTransaction.map(
+    (transaction) => transaction.ammout
+  );
+  const sum = sumArray(transactionAmounts);
+
+  // Atualizando os Incomes
+  const incomeList = transactionAmounts.filter((ammout) => ammout > 0);
+  const income = sumArray(incomeList);
+
+  // Atualizando os Expenses
+  const expenseList = transactionAmounts.filter((ammout) => ammout < 0);
+  const expense = sumArray(expenseList);
+
+  //Atualizando a UI
+  balance.innerHTML = sum < 0 ? `- R$${Math.abs(sum)}` : `R$${sum}`;
+  incomeUI.innerHTML = `+ R$${income}`;
+  expenseUI.innerHTML = `- R$${expense}`;
+};
+
+// Iniciando a aplicação
 const init = () => {
   dummyTransaction.forEach(addTransactionIntoDOM);
-}
+  updateBalanceValues();
+};
 
 init();
